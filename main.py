07e97c623bafdf9e66b65cbd94eb9b447ed3e2e9
iamwors <133743340+iamwors@users.noreply.github.com>
2024-01-08 18:03:05 +0530

    create guessing game

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+def main() -> None:
+    print("\n\tGuess the number between 0 to 100 . You have 7 chances.\n\tGood Luck!.")
+    NUMBER = int(random.randint(0,100))
+    CHANCES = 7
+    Count = 0 
+    while Count < CHANCES:
+        guess = int(input(f"\nLIFE [{"💖 "*(CHANCES - Count)}{"❌ "*Count}] Guess the Number : "))
+        Count += 1
+        if guess > NUMBER:
+            print("Your guess is too high.")
+        elif guess < NUMBER:
+            print("Your guess is too small.")
+        else:
+            return print("Your guess is correct.\n\tYOU WIN!")
+    print("GAME OVER")
+
+if __name__ == "__main__":
+    main()
