3129a54c1efabae65df262e49a0cb25c5e5d0864
iamwors <133743340+iamwors@users.noreply.github.com>
2024-01-09 18:23:05 +0530

    Refactor remove extra lines

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import random
 
-
 def main() -> None:
-    print("\n\tGuess the number between 0 to 100 . You have 7 chances.\n\tGood Luck!.")
     NUMBER = int(random.randint(0,100))
     CHANCES = 7
     Count = 0 
+    
+    print("\n\tGuess the number between 0 to 100 . You have 7 chances.\n\tGood Luck!.")
+    
     while Count < CHANCES:
         guess = int(input(f"\nLIFE [{"💖 "*(CHANCES - Count)}{"❌ "*Count}] Guess the Number : "))
         Count += 1
+        
         if guess > NUMBER:
             print("Your guess is too high.")
         elif guess < NUMBER:
             print("Your guess is too small.")
         else:
             return print("Your guess is correct.\n\tYOU WIN!")
+    
     print("GAME OVER")
 
 if __name__ == "__main__":
